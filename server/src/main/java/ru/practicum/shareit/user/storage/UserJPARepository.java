01a1d5a14d8f36b9098352e7aca1d3e05a495d9c
German Davydov <dawydow.german@yandex.ru>
2025-12-31 09:19:29 +0300

    feat: add existsByEmail method to UserJPARepository

--- server/src/main/java/ru/practicum/shareit/user/storage/UserJPARepository.java
+++ server/src/main/java/ru/practicum/shareit/user/storage/UserJPARepository.java
@@ -1,9 +1,13 @@
 package ru.practicum.shareit.user.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.shareit.user.model.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserJPARepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM users WHERE email = :email)", nativeQuery = true)
+    boolean existsByEmail(@Param("email") String email);
 }
