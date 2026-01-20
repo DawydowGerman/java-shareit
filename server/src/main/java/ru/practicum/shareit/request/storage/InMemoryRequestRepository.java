1b6613d7e46c988c8237f4bc3f4094cdd725d99f
German Davydov <dawydow.german@yandex.ru>
2026-01-20 07:43:59 +0300

    feat: implement update method in InMemoryRequestRepository class

--- server/src/main/java/ru/practicum/shareit/request/storage/InMemoryRequestRepository.java
+++ server/src/main/java/ru/practicum/shareit/request/storage/InMemoryRequestRepository.java
@@ -3,6 +3,8 @@ package ru.practicum.shareit.request.storage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.expection.InternalServerException;
+import ru.practicum.shareit.expection.NotFoundException;
 import ru.practicum.shareit.request.controller.RequestController;
 import ru.practicum.shareit.request.model.Request;
 
@@ -27,6 +29,19 @@ public class InMemoryRequestRepository {
         }
         return new ArrayList<>(requests.values());
     }
+
+    public Request update(Request newRequest) {
+        if (!requests.containsKey(newRequest.getId())) {
+            throw new NotFoundException("Запрос с id " + newRequest.getId() + " отсутствует.");
+        }
+        Request oldRequest = requests.get(newRequest.getId());
+        if (newRequest.getDescription() != null && !newRequest.getDescription().equals(oldRequest.getDescription())) {
+            oldRequest.setDescription(newRequest.getDescription());
+        }
+        log.debug("Обновлен запрос с Id {}", newRequest.getId());
+        return oldRequest;
+    }
+
 
     private long getId() {
         long lastId = requests.values().stream()
